83c08bde610b637f581dfd2df053688432d1c6e9
Harig16 <95hari.gopal@gmail.com>
2022-03-18 00:18:10 +0530

    Add cart, product and line item relation, create session for cart, Add button to corresponding product to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+
+  include PageCounter
+  before_action :index_counter, only: [:index]
+
   def index
     @products = Product.order(:title)
+    @count = session[:counter]
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item #custom validation
+
   validates :title, :description, :image_url, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true, length: { minimum: 10 }
@@ -7,4 +12,14 @@ class Product < ApplicationRecord
                 with: %r{\.(jpg|gif|png)\z}i,
                 message: "must be a URL for GIF, JPG or PNG image."
             }
+  
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, "Line item present") #error object directly refereing to base obj, it can refer individual attributes
+        throw :abort
+      end
+    end
+  
 end
