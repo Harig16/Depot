1e317d249aa8fe2bb93d6ca51e6a1ac71fcffc6d
Harig16 <95hari.gopal@gmail.com>
2022-04-10 21:53:20 +0530

    Capturing order

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,8 +1,12 @@
 class LineItem < ApplicationRecord
   belongs_to :product
-  belongs_to :cart
+  belongs_to :cart, optional: true
+  belongs_to :order, optional: true
 
   def total_price
     product.price * quantity
   end
+
+  # Query the product detail with corresponding line_items and orders
+  #select line_items.product_id, products.title, products.price, line_items.price from line_items INNER JOIN products on line_items.product_id = products.id where exists (select orders.id from orders where line_items.order_id = orders.id);
 end
