f436da4454bebc2edfa777e58bab7c7d54409dc8
Harig16 <95hari.gopal@gmail.com>
2022-04-20 18:41:48 +0530

    Update order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -64,6 +64,18 @@ class OrdersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def pay_type_params
+      if order_params[:pay_type] == "Credit Card"
+        params.require(:order).permit(:credit_card_number, :expire_date)
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "Purchase Order"
+        params.require(:order).permit(:po_number)
+      else
+        {}
+      end
+    end
+    
     def set_order
       @order = Order.find(params[:id])
     end
@@ -76,18 +88,6 @@ class OrdersController < ApplicationController
     def ensure_cart_isnt_empty
       if @cart.line_items.empty?
         redirect_to store_index_url, notice:  "Your Cart is Empty"
-      end
-    end
-
-    def pay_type_params
-      if order_params[:pay_type] == "Credit Card"
-        params.require(:order).permit(:credit_card_number, :expire_date)
-      elsif order_params[:pay_type] == "Check"
-        params.require(:order).permit(:routing_number, :account_number)
-      elsif order_params[:pay_type] == "Purchase Order"
-        params.require(:order).permit(:po_number)
-      else
-        {}
       end
     end
 end
