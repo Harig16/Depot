c8ca4ec1eea80890365ea7cd669384d367ddccad
Harig16 <95hari.gopal@gmail.com>
2022-05-04 19:40:00 +0530

    Implement Session and Admin controller and update system user and session controller test

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      redirect_to admin_url, alert: "User logged in successfully!"
+    else
+      redirect_to login_url, alert: "Invalid Username/Password combination"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil 
+    redirect_to store_index_url, alert: "User logout successfully"
+  end
+end
