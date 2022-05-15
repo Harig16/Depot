46d7f4b12ad4aa7bc1fbe11f7f5733a85aaa1e5f
Harig16 <95hari.gopal@gmail.com>
2022-05-15 22:48:38 +0530

    Creating support request from our mailbox

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,9 +1,11 @@
 class SupportMailbox < ApplicationMailbox
   def process
-    puts "START SupportMailBox#process:"
-    puts "From    :  #{mail.from_address}"
-    puts "Subject :  #{mail.subject}"
-    puts "Body :  #{mail.body}"
-    puts "END SupportMailBox#process:"
+    recent_order = Order.where(email: mail.from_address.to_s).order('created_at DESC').first
+    SupportRequest.create!(
+      email:  mail.from_address.to_s,
+      subject:  mail.subject,
+      body:  mail.body.to_s,
+      order: recent_order
+    )
   end
 end
