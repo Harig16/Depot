1e17315f91e04d7e6c8f9a26e563fe62d7f8dc51
Harig16 <95hari.gopal@gmail.com>
2022-05-15 21:35:24 +0530

    Create model for support request

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_15_124723) do
+ActiveRecord::Schema.define(version: 2022_05_15_155940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 2022_05_15_124723) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "support_requests", force: :cascade do |t|
+    t.string "email", comment: "Email of the submitter"
+    t.string "subject", comment: "Subject of their support email"
+    t.text "body", comment: "Body of their support email"
+    t.bigint "order_id", comment: "their most recent order, if applicable"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_support_requests_on_order_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
@@ -93,4 +103,5 @@ ActiveRecord::Schema.define(version: 2022_05_15_124723) do
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
+  add_foreign_key "support_requests", "orders"
 end
