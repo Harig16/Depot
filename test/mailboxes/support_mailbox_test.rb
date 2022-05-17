620c92001e9380cd54a75a257478c3899bdf72f6
Harig16 <95hari.gopal@gmail.com>
2022-05-17 19:06:36 +0530

    Test support request with recent order

--- test/mailboxes/support_mailbox_test.rb
+++ test/mailboxes/support_mailbox_test.rb
@@ -8,4 +8,37 @@ class SupportMailboxTest < ActionMailbox::TestCase
   #     subject: "Hello world!",
   #     body: "Hello?"
   # end
+
+  test "create SupportRequest when we get email" do
+    receive_inbound_email_from_mail(
+      to: "support@example.com",
+      from: "abc@example.com",
+      subject: "Need help",
+      body: "I can't figure it out how to checkout"
+    )
+    support_requests = SupportRequest.last
+    assert_equal "abc@example.com", support_requests.email
+    assert_equal "Need help", support_requests.subject
+    assert_equal "I can't figure it out how to checkout", support_requests.body
+    assert_nil support_requests.order
+  end
+
+  test "create SupportRequest with recent orders" do
+
+    recent_order = orders(:one)
+    old_order = orders(:another_one)
+    no_customer_order = orders(:other_customer)
+
+    receive_inbound_email_from_mail(
+      to: "support@example.com",
+      from: recent_order.email,
+      subject: "Need help",
+      body: "I can't figure it out how to checkout"
+    )
+    support_requests = SupportRequest.last
+    assert_equal recent_order.email, support_requests.email
+    assert_equal "Need help", support_requests.subject
+    assert_equal "I can't figure it out how to checkout", support_requests.body
+    assert_equal recent_order, support_requests.order
+  end
 end
